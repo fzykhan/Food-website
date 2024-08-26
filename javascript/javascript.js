da467f76ad56189bfcefd66833f8f09dca9709d7
Faizan Khan <KMCFK013@dawateislami.lan>
2024-08-26 19:18:42 +0500

    good

---
+++ javascript/javascript.js
@@ -0,0 +1,28 @@
+/*
+let num = 5;
+num = 10;
+console.log('ans is: \t' + num);
+*/
+
+/*
+//format specifier
+let num = 5;
+console.log('there are %d item in your basket' + num );
+*/
+
+/*
+//backticks
+let num = 5;
+let message = `there are ${num} item in your basket.`;
+console.log(message);
+*/
+  
+
+let student = 
+{ 
+    firstName: "Muhammad",
+    lastName: "Faizan",
+    count: 786
+};
+
+console.log( "student name is:\t" + student.count);
